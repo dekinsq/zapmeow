bc65387121b9f93800ed39034a97fd1b63d3b9c6
qianbin <qianbin@xintai-spc.com>
2025-03-02 16:11:33 +0800

    add proxy

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
-CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o app-linux -ldflags="-s -w" main.go
-CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 go build -o app-mac -ldflags="-s -w" main.go
+CGO_ENABLED=1 GOOS=linux GOARCH=amd64 go build -o app-linux -ldflags="-s -w" main.go
+CGO_ENABLED=1 GOOS=darwin GOARCH=amd64 go build -o app-mac -ldflags="-s -w" main.go
 
